85d917142f06db46816878d7c7acb6d259c7b857
Stageddat <chmarc72@gmail.com>
2025-09-27 21:27:26 +0200

    fix: remove useless tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	entry: ['./src/index.ts'],
-	format: 'esm',
+	format: 'cjs',
 	clean: true,
 	minify: true,
+	keepNames: true,
+	treeshake: true,
 	outDir: 'dist',
 	bundle: true,
+	platform: 'node',
+	target: 'node16',
 	esbuildOptions(options) {
 		options.alias = {
 			'@': './src',
@@ -14,4 +18,6 @@ export default defineConfig({
 	},
 	dts: false,
 	sourcemap: false,
+	noExternal: [/(.*)/],
+	splitting: false,
 });
