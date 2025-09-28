678e2d1704be47abe8a37b6b92633833c05e42d2
Stageddat <chmarc72@gmail.com>
2025-09-29 01:35:40 +0200

    refactor: remove useless

--- src/enum/codeStatus.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum codeStatus {
-	codeFound = 'Code found in the database',
-	codeNotFound = 'Code not found in the database',
-	codeRegistered = 'Code registered in the database',
-	newCodeRegistered = 'New code registered in the database',
-	codeNotAvaible = 'Code not avaible in the database',
-	codeExpired = 'Code expired in the database',
-}
