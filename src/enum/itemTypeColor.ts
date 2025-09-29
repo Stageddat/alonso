f020233ab62d76a36c831fcf932fef033a0983bc
Stageddat <chmarc72@gmail.com>
2025-09-29 10:31:07 +0200

    feat: add course hex colors

---
+++ src/enum/itemTypeColor.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+export enum itemTypeColor {
+	SASP = '7ed957',
+	SGE = 'e2a9f1',
+	PRO = '5ce1e6',
+	IPO1 = '5271ff',
+	SI = 'bfecac',
+	BD = 'ff9999',
+	ANG = 'ffde59',
+	TUT = 'ffbd59',
+	DASP = 'cb6ce6',
+	AD = 'ff751f',
+	LMSGI = 'ff3a3a',
+	ED = 'ff66c4',
+}
