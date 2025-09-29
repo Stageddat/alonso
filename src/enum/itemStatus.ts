21b26b95a4a4b52ea32b2ef72b08de250eac9096
Stageddat <chmarc72@gmail.com>
2025-09-29 10:29:59 +0200

    feat: add item db enum

---
+++ src/enum/itemStatus.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+export enum itemStatus {
+	itemFound = 'Item found in the database',
+	itemNotFound = 'Item not found in the database',
+	itemRegistered = 'Item registered in the database',
+	newItemRegistered = 'New item registered in the database',
+	itemUpdated = 'Item updated in the database',
+	itemIsEqual = 'Item is equal to the database',
+}
