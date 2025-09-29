c5f027690f01107e93e6b33a451dba4d057cd72e
Stageddat <chmarc72@gmail.com>
2025-09-29 10:29:45 +0200

    test: add test command to superusers

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,11 @@
 import pingCommand from './utils/ping.js';
 import helpCommand from './support/ayuda.js';
 import statusCommand from './utils/status.js';
+import testCommand from './utils/test.js';
 
 export const commands = {
 	ping: pingCommand,
 	ayuda: helpCommand,
 	status: statusCommand,
+	test: testCommand,
 };
---
+++ src/commands/utils/test.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import { ItemModel } from '@/models/itemModel';
+import { superUsers } from '@/config/superUsers.js';
+import { notAllowedEmbed } from '@/views/generalEmbeds.js';
+
+const testCommand = {
+	data: new SlashCommandBuilder().setName('test').setDescription('say gex'),
+	async execute(interaction: CommandInteraction) {
+		if (!superUsers.includes(interaction.user.id)) {
+			return interaction.reply({
+				embeds: [notAllowedEmbed],
+				flags: MessageFlags.Ephemeral,
+				withResponse: true,
+			});
+		}
+
+		const item = await ItemModel.getItemByID({ id: 'iwrfhqtk2577a9' });
+		return interaction.reply({ content: `\`\`\`json\n${JSON.stringify(item, null, 2)}\n\`\`\`` });
+	},
+};
+
+export default testCommand;
