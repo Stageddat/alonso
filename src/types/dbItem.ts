d6237d905f2b5c706f957321a4bf205b8a24b130
Stageddat <chmarc72@gmail.com>
2025-09-29 10:29:30 +0200

    feat: add db item type

---
+++ src/types/dbItem.ts
@@ -0,0 +1,13 @@
+export interface DbItem {
+	id: string;
+	title: string;
+	moodle_link: string;
+	notion_link: string;
+	users_completed?: string;
+	due_date: string;
+	item_type: string;
+	course: string;
+	due_today_msg_id?: string;
+	week_msg_id?: string;
+	dm_msg_id?: string;
+}
