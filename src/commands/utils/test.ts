68e0f77ca1cc0ecf1fa7f097dec6f6f938ccb48e
Stageddat <chmarc72@gmail.com>
2025-09-30 14:26:59 +0200

    test: more testing :D

--- src/commands/utils/test.ts
+++ src/commands/utils/test.ts
@@ -1,7 +1,7 @@
 import { CommandInteraction, MessageFlags, SlashCommandBuilder } from 'discord.js';
-import { ItemModel } from '@/models/itemModel';
 import { superUsers } from '@/config/superUsers.js';
 import { notAllowedEmbed } from '@/views/generalEmbeds.js';
+// import { MsgModel } from '@/models/msgModel';
 
 const testCommand = {
 	data: new SlashCommandBuilder().setName('test').setDescription('say gex'),
@@ -17,8 +17,13 @@ const testCommand = {
 		// const item = await ItemModel.getItemByID({ id: 'iwrfhqtk2577a9' });
 		// return interaction.reply({ content: `\`\`\`json\n${JSON.stringify(item, null, 2)}\n\`\`\`` });
 
-		const item = await ItemModel.getTodayItems();
-		return interaction.reply({ content: `\`\`\`json\n${JSON.stringify(item, null, 2)}\n\`\`\`` });
+		// const item = await ItemModel.getTodayItems();
+		// return interaction.reply({ content: `\`\`\`json\n${JSON.stringify(item, null, 2)}\n\`\`\`` });
+
+		// const todayMsgId = await MsgModel.getTodayMsgId();
+		// return interaction.reply({
+		// 	content: `\`\`\`json\n${JSON.stringify(todayMsgId, null, 2)}\n\`\`\``,
+		// });
 	},
 };
 
