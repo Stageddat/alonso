121b83ac5f7d61d8d8295639eab2d1c56d69b737
Stageddat <chmarc72@gmail.com>
2025-10-01 09:10:43 +0200

    refactor: add dateStatus enum

---
+++ src/enum/dateStatus.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export enum dateStatus {
+	dateFound = 'Date found in the database',
+	dateNotFound = 'Date not found in the database',
+	dateAdded = 'Date added in the database',
+}
