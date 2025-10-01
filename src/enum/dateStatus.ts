985f2f557a7798583a3ce2e844d377bcf2ad5252
Stageddat <chmarc72@gmail.com>
2025-10-01 12:40:39 +0200

    feat: add dateUpdated and failedToSendTodayDate enum

--- src/enum/dateStatus.ts
+++ src/enum/dateStatus.ts
@@ -3,4 +3,6 @@ export enum dateStatus {
 	dateFound = 'Date found in the database',
 	dateNotFound = 'Date not found in the database',
 	dateAdded = 'Date added in the database',
+	dateUpdated = 'Date updated in the database',
+	failedToSendTodayDate = 'Failed to send today message',
 }
