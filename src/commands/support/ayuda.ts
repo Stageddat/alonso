7d9122852e2544d6f4d8322dc075e2f67ab3c795
stageddat <chmarc72@gmail.com>
2026-01-19 00:54:13 +0100

    feat: change help support command response

--- src/commands/support/ayuda.ts
+++ src/commands/support/ayuda.ts
@@ -3,7 +3,7 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 const ayudaCommand = {
 	data: new SlashCommandBuilder().setName('ayuda').setDescription('Mostrar ayuda!'),
 	async execute(interaction: CommandInteraction) {
-		return interaction.reply('33');
+		return interaction.reply('nidea bro, pregunta a marc :p');
 	},
 };
 
